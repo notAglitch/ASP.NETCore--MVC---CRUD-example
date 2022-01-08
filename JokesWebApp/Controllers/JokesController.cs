b20974247062721c5e615bbcdb4a62ecc5db9593
Sachintha Nirmal <sachintha919@gmail.com>
2022-01-09 01:35:19 +0530

    Add project files.

---
+++ JokesWebApp/Controllers/JokesController.cs
@@ -0,0 +1,181 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using JokesWebApp.Data;
+using JokesWebApp.Models;
+using Microsoft.AspNetCore.Authorization;
+
+namespace JokesWebApp.Controllers
+{
+    public class JokesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public JokesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Jokes
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Joke.ToListAsync());
+        }
+
+        // GET: Jokes/ShowSearchForm - To show the search form
+        public async Task<IActionResult> ShowSearchForm()
+        {
+            return View();//Not going to view any info.Just an empty view
+        }
+
+
+        //Below method returns an item called Task and IActionResult.There can be whole bunch of other things that can be returned when we create views.
+        //In more advanced version of this app we could create a rest Api ,a service that would create json data and that  would be also one of this IActionResult
+        //public async Task<IActionResult> ShowSearchResults(String SearchPhrase)
+
+        // POST: Jokes/ShowSearchResults - To show the search results
+        public async Task<IActionResult> ShowSearchResults(String SearchPhrase) //SearchPhrase is the 'name' in the form input field
+        {
+            return View("Index",await _context.Joke.Where( j => j.JokeQuestion.Contains(SearchPhrase)).ToListAsync());//This tells ,we are looking for the index view and the following data with SearchPhrase to go with it (its an Arrow Function)
+        }
+
+        // GET: Jokes/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var joke = await _context.Joke
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (joke == null)
+            {
+                return NotFound();
+            }
+
+            return View(joke);
+        }
+
+        // GET: Jokes/Create
+        //We don't want to show this create part until they've logged in, so we're going to put in a decorator or one of these indicators in the sq brackets[Authorize]
+        //include the library Microsoft.AspNetCore.Authorization
+        [Authorize]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Jokes/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+
+        [Authorize]//Also put Authorization here because this is going to actually put data in the DB.This is more important for the security than the entry form
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,JokeQuestion,JokeAnswer")] Joke joke)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(joke);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(joke);
+        }
+
+        // GET: Jokes/Edit/5
+        [Authorize]//Not let anyone edit a joke unless they've been authorized
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var joke = await _context.Joke.FindAsync(id);
+            if (joke == null)
+            {
+                return NotFound();
+            }
+            return View(joke);
+        }
+
+        // POST: Jokes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [Authorize] //Authorize here also
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,JokeQuestion,JokeAnswer")] Joke joke)
+        {
+            if (id != joke.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(joke);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!JokeExists(joke.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(joke);
+        }
+
+        // GET: Jokes/Delete/5
+        [Authorize]//Authorize here also when try  to deleting
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var joke = await _context.Joke
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (joke == null)
+            {
+                return NotFound();
+            }
+
+            return View(joke);
+        }
+
+        // POST: Jokes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        [Authorize]//Authorize when confirming the deletion
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var joke = await _context.Joke.FindAsync(id);
+            _context.Joke.Remove(joke);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool JokeExists(int id)
+        {
+            return _context.Joke.Any(e => e.Id == id);
+        }
+    }
+}
